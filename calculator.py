34f011dade451369de920b404148441db95dad87
shraddha acharya <shraddhaacharya777@email.com>
2026-02-25 21:28:08 +0530

    added continuous loop feature

--- calculator.py
+++ calculator.py
@@ -1,30 +1,34 @@
 print("Simple Calculator")
+while True:
+  num1 = float(input("Enter first number: "))
+  num2 = float(input("Enter second number: "))
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+  print("Choose operation:")
+  print("1. Add")
+  print("2. Subtract")
+  print("3. Multiply")
+  print("4. Divide")
 
-print("Choose operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+  choice = input("Enter choice (1/2/3/4): ")
 
-choice = input("Enter choice (1/2/3/4): ")
+  if choice == '1':
+     print("Result:", num1 + num2)
 
-if choice == '1':
-    print("Result:", num1 + num2)
+  elif choice == '2':
+     print("Result:", num1 - num2)
 
-elif choice == '2':
-    print("Result:", num1 - num2)
+  elif choice == '3':
+     print("Result:", num1 * num2)
 
-elif choice == '3':
-    print("Result:", num1 * num2)
-
-elif choice == '4':
-    if num2 != 0:
+  elif choice == '4':
+     if num2 != 0:
         print("Result:", num1 / num2)
-    else:
+     else:
         print("Cannot divide by zero")
 
-else:
-    print("Invalid input please try again")
+  else:
+     print("Invalid input please try again")
+  again=input("Do you want to calculate again??(yes/no):")
+  if again.lower()!="yes":
+     print("Thank you for using the calculater!")
+     break
